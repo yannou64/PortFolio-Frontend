c1b0a3d6ee17ec550b5a2b2a4ec9e196f49df163
yannou64 <yannickpro64@gmail.com>
2025-09-04 16:00:52 +0200

    Réglage bug de casse sur BienvenueEdition au déploiement

--- src/pages/Body/Edition/Formulaires/BienvenueEdition/BienvenueEdition.jsx
+++ src/pages/Body/Edition/Formulaires/BienvenueEdition/BienvenueEdition.jsx
@@ -1,11 +1,11 @@
-import logo from "../../../../../assets/avatar.png"
-import "./BienvenueEdition.scss"
+import logo from "../../../../../assets/avatar.png";
+import "./bienvenueEdition.scss";
 
-export default function BienvenueEdition(){
-    return (
-        <div className="container_bienvenu_edition">
-            <img src={logo} alt="Image de l'avatar de yannick biot" />
-            <p>Editeur de contenu</p>
-        </div>
-    )
+export default function BienvenueEdition() {
+  return (
+    <div className="container_bienvenu_edition">
+      <img src={logo} alt="Image de l'avatar de yannick biot" />
+      <p>Editeur de contenu</p>
+    </div>
+  );
 }
