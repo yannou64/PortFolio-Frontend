a83d1ce7cb1c93fb05e503bd2d42df69e64bc481
yannou64 <yannickpro64@gmail.com>
2025-09-09 10:30:23 +0200

    bug Header résolu

--- src/pages/Header/Header.jsx
+++ src/pages/Header/Header.jsx
@@ -16,6 +16,7 @@ export default function Header() {
   ////
   const iconeSize = 40;
   const navigate = useNavigate();
+
   ////
   // Gestion isAdmin
   ////
@@ -86,8 +87,8 @@ export default function Header() {
   }, [isMobile]);
 
   // Animation du menuBurger et gestion de l'affichage du navigateur à chaque clique sur le menu
-  useEffect(() => {
-    console.log("isMobile : ", isMobile);
+  function handleBurgerMenu(){
+   
     if (isMobile) {
       burger_icone.current.classList.toggle("open");
       if (burger_icone.current.classList.contains("open")) {
@@ -96,7 +97,8 @@ export default function Header() {
         nav.current.style.display = "none";
       }
     }
-  }, [isMenuBurgerOpen]);
+    setIsMenuBurgerOpen(!isMenuBurgerOpen)
+  }
 
   ////
   // Gestion du cas ou un Admin n'est pas sur desktop
@@ -121,7 +123,7 @@ export default function Header() {
         </a>
       </button>
       {/* Le bouton burger est visible en dessous de 768 px */}
-      <button id="burger-menu" onClick={() => setIsMenuBurgerOpen(!isMenuBurgerOpen)}>
+      <button id="burger-menu" onClick={handleBurgerMenu}>
         <span ref={burger_icone} className="burger-icone">
           <div className="burger-line"></div>
           <div className="burger-line"></div>
@@ -133,7 +135,7 @@ export default function Header() {
         <ul>
           {/* Projets */}
           {!isAdmin && (
-            <li onClick={() => setIsMenuBurgerOpen(!isMenuBurgerOpen)}>
+            <li onClick={handleBurgerMenu}>
               <a href="#liste_projetsFavoris">
                 <GrProjects id="menu_portfolio" className="icone" size={iconeSize - 6} />
                 <p>Projets réalisés</p>
@@ -142,7 +144,7 @@ export default function Header() {
           )}
           {/* Technos */}
           {!isAdmin && (
-            <li onClick={() => setIsMenuBurgerOpen(!isMenuBurgerOpen)}>
+            <li onClick={handleBurgerMenu}>
               <a href="#MesTechnos">
                 <GiStarsStack id="menu_portfolio" className="icone" size={iconeSize - 6} />
                 <p>Stack technique</p>
@@ -151,7 +153,7 @@ export default function Header() {
           )}
           {/* CV */}
           {!isAdmin && (
-            <li onClick={() => setIsMenuBurgerOpen(!isMenuBurgerOpen)}>
+            <li onClick={handleBurgerMenu}>
               <a href="../../public/Documents/cv-yannick-biot.pdf" target="_blank">
                 <TbFileCv
                   id="menu_cv"
@@ -178,7 +180,7 @@ export default function Header() {
           )}
           {/* Contact */}
           {isAdmin !== true && (
-            <li onClick={() => setIsMenuBurgerOpen(!isMenuBurgerOpen)}>
+            <li onClick={handleBurgerMenu}>
               <a href="#Contact">
                 <TfiEmail id="menu_contact" className="icone" size={iconeSize} />
                 <p>Me contacter</p>
