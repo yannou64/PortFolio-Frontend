f86cea354e5926d7dbcda656c8ae2b9598a2d735
yannou64 <yannickpro64@gmail.com>
2025-09-17 16:06:53 +0200

    réparation du lien CG et date dynamique

--- src/pages/Footer/Footer.jsx
+++ src/pages/Footer/Footer.jsx
@@ -1,16 +1,26 @@
-import "./footer.scss"
-import Reseaux from "../components/Reseaux/Reseaux"
-import {useNavigate} from "react-router-dom"
+import "./footer.scss";
+import Reseaux from "../components/Reseaux/Reseaux";
+import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 
-export default function Footer(){
-    const navigate = useNavigate()
+export default function Footer() {
+  const navigate = useNavigate();
+  const [year, setYear] = useState(new Date().getFullYear());
 
-    return (
-        <footer>
-            <p>Biot Yannick | 2025 -   <a href="" onClick={()=>navigate("/conditionsGenerales")}>Conditions Générales</a></p>
-            <Reseaux dark={true}/>
-          
-            <div>Icons made from <a href="https://www.onlinewebfonts.com/icon">svg icons</a>is licensed by CC BY 4.0</div>
-        </footer>
-    )
+  useEffect(() => {
+    // setYear(new Date().getFullYear())
+  }, []);
+
+  return (
+    <footer>
+      <p>
+        Biot Yannick | {year} - <a onClick={() => navigate("/conditionsGenerales")}>Conditions Générales</a>
+      </p>
+      <Reseaux dark={true} />
+
+      <div>
+        Icons made from <a href="https://www.onlinewebfonts.com/icon">svg icons</a>is licensed by CC BY 4.0
+      </div>
+    </footer>
+  );
 }
