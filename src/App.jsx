61b2971a39d997d3f454236fa090ecbfb65ae920
yannou64 <yannickpro64@gmail.com>
2025-09-17 15:48:53 +0200

    mise en place des conditions générales

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Login from "./pages/Body/Login/Login";
 import ErrorPage from "./pages/Body/ErrorPage/ErrorPage";
 import Logout from "./pages/Body/Logout/Logout";
 import Portfolio from "./pages/Body/Portfolio/Portfolio";
+import CG from "./pages/Body/ConditionsGenerales/CG";
 import Edition from "./pages/Body/Edition/Edition";
 import Remerciement from "./pages/Body/Remerciement/Remerciement";
 import Footer from "./pages/Footer/Footer";
@@ -14,31 +15,31 @@ import AuthContext from "./context/AuthContext";
 import { useEffect } from "react";
 
 function App() {
-  const [isAdmin, setIsAdmin] = useState(false)
-  const value = {isAdmin, setIsAdmin}
+  const [isAdmin, setIsAdmin] = useState(false);
+  const value = { isAdmin, setIsAdmin };
 
   // Vérification si un token admin existe et si il n'est pas expiré
-  async function isAlreadyAdmin(){
-    try{
+  async function isAlreadyAdmin() {
+    try {
       const response = await fetch(import.meta.env.VITE_API_URL + "/api/auth/checkIfAdmin", {
         method: "GET",
-        credentials: "include"
-      })
-      const data = await response.json()
-      if(response.ok){
-        if(data.auth) setIsAdmin(true)
-        console.log(data.message)
+        credentials: "include",
+      });
+      const data = await response.json();
+      if (response.ok) {
+        if (data.auth) setIsAdmin(true);
+        console.log(data.message);
       } else {
-        console.log("Error : ", data.message)
+        console.log("Error : ", data.message);
       }
     } catch (e) {
-      console.log("Error : ", e.message)
+      console.log("Error : ", e.message);
     }
   }
-  
-  useEffect(()=>{
-    isAlreadyAdmin()
-  }, [])
+
+  useEffect(() => {
+    isAlreadyAdmin();
+  }, []);
 
   return (
     <AuthContext.Provider value={value}>
@@ -50,6 +51,7 @@ function App() {
             <Route path="/portfolio" element={<Portfolio />} />
             <Route path="/edition" element={<Edition />} />
             <Route path="/register" element={<Register />} />
+            <Route path="/conditionsGenerales" element={<CG />} />
             <Route path="/login" element={<Login />} />
             <Route path="/logout" element={<Logout />} />
             <Route path="/error/:message" element={<ErrorPage />} />
--- src/pages/Footer/Footer.jsx
+++ src/pages/Footer/Footer.jsx
@@ -1,11 +1,15 @@
 import "./footer.scss"
 import Reseaux from "../components/Reseaux/Reseaux"
+import {useNavigate} from "react-router-dom"
 
 export default function Footer(){
+    const navigate = useNavigate()
+
     return (
         <footer>
-            <p>Biot Yannick | 2025</p>
+            <p>Biot Yannick | 2025 -   <a href="" onClick={()=>navigate("/conditionsGenerales")}>Conditions Générales</a></p>
             <Reseaux dark={true}/>
+          
             <div>Icons made from <a href="https://www.onlinewebfonts.com/icon">svg icons</a>is licensed by CC BY 4.0</div>
         </footer>
     )
