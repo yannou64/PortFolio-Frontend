43fcdbdacd6e51aa6510ea048b2cdb9c35d14105
yannou64 <yannickpro64@gmail.com>
2025-09-19 17:44:58 +0200

    modification du style de Remerciement

--- src/pages/Body/Remerciement/Remerciement.jsx
+++ src/pages/Body/Remerciement/Remerciement.jsx
@@ -1,22 +1,23 @@
-import "./remerciement.scss"
-import {useNavigate} from "react-router-dom"
-import {useEffect} from "react"
-import myAvatar from "../../../assets/avatar.webp"
+import "./remerciement.scss";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import myAvatar from "../../../assets/avatar.webp";
 
+export default function Remerciement() {
+  const navigate = useNavigate();
 
-export default function Remerciement(){
-    const navigate = useNavigate()
+  useEffect(() => {
+    setTimeout(()=>{
+        navigate("/")
+    }, 3000)
+  }, []);
 
-    useEffect(()=>{
-        setTimeout(()=>{
-            navigate("/")
-        }, 3000)
-    }, [])
-
-    return (
-        <div className="container_remerciement">
-            <img src={myAvatar} alt="Avatar du propriétaire de l'application web" />
-            <h1>Merci beaucoup, je vous recontacte rapidement.</h1>
-        </div>
-    )
+  return (
+    <div className="container_remerciement">
+      <div className="image_container">
+        <img src={myAvatar} alt="Avatar du propriétaire de l'application web" />
+      </div>
+      <h1>Merci beaucoup, je vous recontacte rapidement.</h1>
+    </div>
+  );
 }
